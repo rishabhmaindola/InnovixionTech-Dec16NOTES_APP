69be76e5673563f8a2ddf8c9d4a43a9c6998a7af
Rishabh Maindola <rishabhmaindola5@gmail.com>
2023-12-20 11:30:00 +0530

    Initial Commit

---
+++ src/Components/Note/Note.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+import deleteIcon from "../../assets/delete.svg";
+
+import "./Note.css";
+
+let timer = 500,
+  timeout;
+function Note(props) {
+  const formatDate = (value) => {
+    if (!value) return "";
+
+    const date = new Date(value);
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "March",
+      "April",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    let hrs = date.getHours() % 12 || 12;
+    let amPm = date.getHours() >= 12 ? "PM" : "AM";
+    let min = date.getMinutes().toString().padStart(2, "0");
+    let day = date.getDate();
+    const month = monthNames[date.getMonth()];
+
+    return `${hrs}:${min} ${amPm} ${day} ${month}`;
+  };
+
+  const debounce = (func) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, timer);
+  };
+
+  const updateText = (text, id) => {
+    debounce(() => props.updateText(text, id));
+  };
+
+  return (
+    <div className="note" style={{ backgroundColor: props.note.color }}>
+      <textarea
+        className="note_text"
+        defaultValue={props.note.text}
+        onChange={(event) => updateText(event.target.value, props.note.id)}
+      />
+      <div className="note_footer">
+        <p>{formatDate(props.note.time)}</p>
+        <img
+          src={deleteIcon}
+          alt="DELETE"
+          onClick={() => props.deleteNote(props.note.id)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Note;
